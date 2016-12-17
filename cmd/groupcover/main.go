289e54c2982ea037ccb1a5d7a0bbbc70479e2f5d
Martin Czygan <martin.czygan@gmail.com>
2016-12-17 12:16:12 +0100

    sample rewriter

--- cmd/groupcover/main.go
+++ cmd/groupcover/main.go
@@ -1,39 +1,90 @@
 package main
 
 import (
-	"bufio"
 	"encoding/csv"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
-// sort -t ',' -k 3 fixtures/sample.tsv
-func main() {
-	r := csv.NewReader(bufio.NewReader(os.Stdin))
-	r.Comma = ','
+// Attr extract an attribute from a CSV record.
+type AttrFunc func([]string) (string, error)
 
-	var batch []string
+// RewriterFunc rewrites a list of lines.
+type RewriterFunc func([][]string) ([][]string, error)
+
+// GroupLines takes a reader (over CSV) and a attribtue value extractor and
+// groups lines that have the same attribute. As with uniq, the lines must be
+// sorted by the attribute.
+func GroupLines(r io.Reader, w io.Writer, attrFunc AttrFunc, rewriterFunc RewriterFunc) error {
+	cr := csv.NewReader(r)
+	cw := csv.NewWriter(w)
+
+	var prev string
+	var group [][]string
 
 	for {
-		record, err := r.Read()
+		record, err := cr.Read()
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
-		if len(batch) == 0 {
-			batch = append(batch, record[2])
-			continue
+		attr, err := attrFunc(record)
+		if err != nil {
+			return err
 		}
-		if record[2] != batch[len(batch)-1] {
-			log.Println("processing batch of %d items", len(batch))
-			log.Println(batch)
-			batch = nil
+		if attr != prev {
+			regroup, err := rewriterFunc(group)
+			if err != nil {
+				return err
+			}
+			if err := cw.WriteAll(regroup); err != nil {
+				return err
+			}
+			group = nil
 		}
-		batch = append(batch, record[2])
+		group = append(group, record)
+		prev = attr
 	}
 
-	log.Println("rest", batch)
+	// final group
+	regroup, err := rewriterFunc(group)
+	if err != nil {
+		return err
+	}
+	return cw.WriteAll(regroup)
+}
+
+func main() {
+	// extract the third column
+	thirdColumn := func(record []string) (string, error) {
+		if len(record) != 4 {
+			return "", fmt.Errorf("invalid column count: got %d, want 4", len(record))
+		}
+		return record[2], nil
+	}
+
+	// deduplicate keys
+	sampleRewriter := func(s [][]string) ([][]string, error) {
+		if len(s) < 2 {
+			return s, nil
+		}
+		// TODO(miku):
+		// 1. For each ISIL (keys) get the available groups.
+		// 2. For each ISIL (keys) get the preferred group.
+		// 3. For each lines, check the group and list the ISIL (keys) for which this group is the preferred.
+		for _, line := range s {
+			keys := strings.Split(line[3], ",")
+			log.Println(keys)
+		}
+		return s, nil
+	}
+
+	if err := GroupLines(os.Stdin, os.Stdout, thirdColumn, sampleRewriter); err != nil {
+		log.Fatal(err)
+	}
 }
