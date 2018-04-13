3739d06991838d1e0d75136329ac58eddcff55ee
Martin Czygan <martin.czygan@gmail.com>
2018-04-13 14:06:22 +0200

    fix comment

--- cmd/groupcover/main.go
+++ cmd/groupcover/main.go
@@ -1,4 +1,4 @@
-//  Copyright 2016 by Leipzig University Library, http://ub.uni-leipzig.de
+// Copyright 2016 by Leipzig University Library, http://ub.uni-leipzig.de
 //                    The Finc Authors, http://finc.info
 //                    Martin Czygan, <martin.czygan@uni-leipzig.de>
 //
