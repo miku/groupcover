c3fcf548fef41a90462a107d54ea89d6df53c3c7
Martin Czygan <martin.czygan@gmail.com>
2019-03-20 10:42:17 +0100

    select response

--- cmd/groupindex/main.go
+++ cmd/groupindex/main.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -23,6 +24,42 @@ var (
 type WithLabels struct {
 	Labels []string `json:"x.labels"`
 	DOI    string   `json:"doi"`
+}
+
+// SelectResponse with reduced fields.
+type SelectResponse struct {
+	Response struct {
+		Docs []struct {
+			Institution []string `json:"institution"`
+			SourceId    string   `json:"source_id"`
+		} `json:"docs"`
+		NumFound int64 `json:"numFound"`
+		Start    int64 `json:"start"`
+	} `json:"response"`
+	ResponseHeader struct {
+		Params struct {
+			Q  string `json:"q"`
+			Wt string `json:"wt"`
+		} `json:"params"`
+		QTime  int64
+		Status int64 `json:"status"`
+	} `json:"responseHeader"`
+}
+
+// fetchDocuments fetches documents for a given doi.
+func fetchDocuments(doi string) (*SelectResponse, error) {
+	// http://172.18.113.7:8085/solr/biblio/select?wt=json&q="10.17145/jab.18.002
+	link := fmt.Sprintf(`http://172.18.113.7:8085/solr/biblio/select?wt=json&q="%s"`, doi)
+	resp, err := http.Get(link)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var sr SelectResponse
+	if err := json.NewDecoder(resp.Body).Decode(&sr); err != nil {
+		return nil, err
+	}
+	return &sr, nil
 }
 
 func main() {
@@ -60,6 +97,11 @@ func main() {
 		if strings.TrimSpace(doc.DOI) == "" {
 			continue
 		}
-		fmt.Println(doc.DOI)
+		sr, err := fetchDocuments(doc.DOI)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		fmt.Printf("%s => %d\n", doc.DOI, sr.Response.NumFound)
 	}
 }
